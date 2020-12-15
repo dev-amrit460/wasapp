6bf2e638caa1b2d036546da0e026b2a6ae9d7d86
dev-amrit460 <go4amrit460@gmail.com>
2020-12-15 22:47:38 +0530

    express

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const io= require('socket.io')(5000)
+const express = require('express')
+const app=express()
+const server= require('http').Server(app)
+const io= require('socket.io')(server)
+
+const PORT=process.env.PORT ||5000
+server.listen(5000, () => {
+    console.log('App listening on port 5000!');
+});
 
 const users={};
 io.on('connection',socket=>{
