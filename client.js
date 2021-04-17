a06f075aa802dd09b0fd350f252ca959e4b869e3
Amrit Raj <55765255+dev-amrit460@users.noreply.github.com>
2021-04-17 11:36:28 +0530

    update

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:5000');
+const socket = io("http://localhost:5000");
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app=express()
-const server= require('http').Server(app)
+const http = require('http');
+const server = http.createServer(app);
 const io= require('socket.io')(server)
 
 const PORT=process.env.PORT ||5000
